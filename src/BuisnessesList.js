48fc49f71a4d71d08b798316cbfa8d1f47ed23bf
Niki Hite <nikihite@comcast.net>
2022-06-29 15:30:14 -0700

    styled pokemon

--- src/BuisnessesList.js
+++ src/BuisnessesList.js
@@ -3,7 +3,7 @@ export default function BuisnessesList({ yelpData }) {
     <div className="businesses-list">
       {
         yelpData &&
-        yelpData.map(({ name, price, phone, image_url }, i) => <div key={name + i}>
+        yelpData.map(({ name, price, phone, image_url }, i) => <div className="yelp" key={name + i}>
           <h2>{name}</h2>
           <p>{price}</p>
           <p>{phone}</p>
