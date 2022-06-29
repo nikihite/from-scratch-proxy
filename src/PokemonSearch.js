93b18314260c15004e4a32a7e62b7c7b78b0eb44
Niki Hite <66445658+nikihite@users.noreply.github.com>
2022-06-29 15:52:39 -0700

    Revert "Dev"

--- src/PokemonSearch.js
+++ src/PokemonSearch.js
@@ -3,28 +3,34 @@ import PokemonList from './PokemonList';
 import { getPokemon } from './services/fetch-utils';
 
 export default function PokemonSearch() {
-  const [pokemon, setPokemon] = useState([]);
+  const [pokemon, setPokemon] = useState('');
   const [pokemonQuery, setPokemonQuery] = useState('');
+
   useEffect(() => {
     load();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   async function load() {
-    const results = await getPokemon(pokemonQuery);
-    setPokemon(results.data.results);
+    const { results } = await getPokemon(pokemonQuery);
+    setPokemon(results);
+  }
+
+  async function loadPokemonSearch() {
+    const pokemon = await getPokemon(pokemonQuery);
+    setPokemon(pokemon);
   }
 
   async function handlePokemonSearch(e) {
     e.preventDefault();
-    await load();
+    loadPokemonSearch();
   }
+
   return (
-    <div className='pokemon-search'>
+    <div>
       <form onSubmit={handlePokemonSearch}>
         <h2>Pokemon</h2>
         <input value={pokemonQuery} onChange={e => setPokemonQuery(e.target.value)} />
-        <button>Submit</button>
       </form>
       <PokemonList pokemon={pokemon}/>
     </div>
--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,13 +1,7 @@
 export async function getPokemon(searchFilter) {
     // we now hit OUR OWN ENDPOINT to get the movies. this lets us hide our key.
-  const rawResponse = await fetch(`/.netlify/functions/pokemon?pokeQuery=${searchFilter}`);
+  const rawResponse = await fetch(`http://localhost:8888/.netlify/functions/pokemon?searchFilter=${searchFilter}`);
   const data = await rawResponse.json();
-  return data;
-}
-
-export async function getYelpData(searchFilter) {
-  const rawResponse = await fetch(`/.netlify/functions/yelp?searchFilter=${searchFilter}`);
-  const data = await rawResponse.json();
-
+  
   return data;
 }
