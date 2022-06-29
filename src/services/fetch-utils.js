6d60824a49b80e6475718b7a5e03726fa4efada1
Niki Hite <nikihite@comcast.net>
2022-06-29 14:33:07 -0700

    created files, added netlify func, and fetch utils for yelp

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -4,3 +4,10 @@ export async function getPokemon(searchFilter) {
   const data = await rawResponse.json();
   return data;
 }
+
+export async function getYelpData(searchFilter) {
+  const rawResponse = await fetch(`/.netlify/functions/yelp?searchFilter=${searchFilter}`);
+  const data = await rawResponse.json();
+
+  return data;
+}
