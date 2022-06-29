6299bf9036dede2e016250630fa82fd83ea89162
Niki Hite <nikihite@comcast.net>
2022-06-28 17:54:38 -0700

    added stuff

---
+++ src/PokemonSearch.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import PokemonList from './PokemonList';
+import { getPokemon } from './services/fetch-utils';
+ 
+export default function PokemonSearch() {
+  const [pokemon, setPokemon] = useState('');
+  const [pokemonQuery, setPokemonQuery] = useState('');
+ 
+  useEffect(() => {
+    load();
+ // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+ 
+  async function load() {
+    const { results } = await getPokemon(pokemonQuery);
+    setPokemon(results);
+  }
+ 
+  async function loadPokemonSearch() {
+    const pokemon = await getPokemon(pokemonQuery);
+    setPokemon(pokemon);
+  }
+ 
+  async function handlePokemonSearch(e) {
+    e.preventDefault();
+    loadPokemonSearch();
+  }
+ 
+  return (
+    <div>
+      <form onSubmit={handlePokemonSearch}>
+        <h2>Pokemon</h2>
+        <input value={pokemonQuery} onChange={e => setPokemonQuery(e.target.value)} />
+      </form>
+      <PokemonList pokemon={pokemon}/>
+    </div>
+  );
+}
